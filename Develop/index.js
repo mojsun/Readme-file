93f003d07e3e78243930cb4e53d01a3e25918a5b
mojsun <m.khorashahi7@gmail.com>
2023-02-09 22:26:18 -0500

    generate questions to the readme file

--- Develop/index.js
+++ Develop/index.js
@@ -14,50 +14,105 @@ init();*/
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "what is the project title?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "write a description of your project:",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "describe the installation process:",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "what is this project usage for",
-  },
-  {
-    type: "input",
-    name: "license",
-    message: "what is the license of this project",
-  },
-  {
-    type: "input",
-    name: "contributors",
-    message: "who are the contributors of this project?",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "is there a test included",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "please enter your github username",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "please enter your email",
-  },
-]);
+const generateReadmeFile = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contributors,
+  test,
+  github,
+  email,
+}) =>
+  `
+# Description
+The title of the project is ${title}\n
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+The discription of the project is ${description} \n
+
+## Installation
+Installation process: ${installation}\n
+
+## Usage
+Usage of the project: ${usage}\n
+
+## License
+license of the project: ${license}\n
+
+##Contributors
+Contributors of this project: ${contributors}\n
+
+##Test
+Is there a test included ${test}\n
+
+##Github
+My Github Username: ${github}\n
+
+##Email
+My Email address${email}\n
+`;
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "what is the project title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "write a description of your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "describe the installation process:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "what is this project usage for",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "what is the license of this project",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "who are the contributors of this project?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "is there a test included",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "please enter your github username",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "please enter your email",
+    },
+  ])
+  .then((answers) => {
+    const ReadmeContent = generateReadmeFile(answers);
+
+    fs.writeFile("readme.md", ReadmeContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created readme!")
+    );
+  });

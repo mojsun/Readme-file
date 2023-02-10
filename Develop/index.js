b03a1cde121b56dbb86c5d56e40d3ead453d2f5e
mojsun <m.khorashahi7@gmail.com>
2023-02-09 21:46:05 -0500

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+/*// TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -10,4 +10,54 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+init();*/
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "what is the project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "write a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "describe the installation process:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "what is this project usage for",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "what is the license of this project",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "who are the contributors of this project?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "is there a test included",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "please enter your github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "please enter your email",
+  },
+]);
